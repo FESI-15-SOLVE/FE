bab5efb0fc3397f5f85453474422727ff86ec622
khr <sykhrb793005@naver.com>
2026-07-15 16:41:14 +0900

    🐛fix: 메인페이지 버튼 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <Button className="bg-blue-500 text-white" variant="ghost">
-          Click me
-        </Button>
-      </main>
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black"></main>
     </div>
   );
 }
