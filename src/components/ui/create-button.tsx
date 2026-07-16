c4fa69f1551953d560f8df5e48ebbe6765e3c982
khr <sykhrb793005@naver.com>
2026-07-16 14:41:04 +0900

    ✨feat: +버튼 기본버튼 사용하도록 수정

--- src/components/ui/create-button.tsx
+++ src/components/ui/create-button.tsx
@@ -1,41 +1,34 @@
-import { Button as ButtonPrimitive } from "@base-ui/react/button";
-import { cva, type VariantProps } from "class-variance-authority";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Button as ButtonPrimitive } from "@base-ui/react/button";
 
 import icPlus from "@/assets/svg/ic-plus.svg";
 import { cn } from "@/lib/utils";
 
-const createButtonVariants = cva(
-  "group/button gap-[6px] flex inline-flex shrink-0 items-center justify-center font-semibold whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-  {
-    variants: {
-      size: {
-        default: "h-[48px] w-[48px] rounded-[50px] bg-green-500",
-        lg: "h-[64px] w-[188px] rounded-[16px] text-xl text-white bg-green-500",
-      },
-    },
-    defaultVariants: {
-      size: "default",
-    },
-  },
-);
+type CreateButtonSize = "small" | "large";
+const createButtonSizeVariants: Record<CreateButtonSize, string> = {
+  small: "h-12 w-12 rounded-full bg-green-500",
+  large: "h-16 w-47 rounded-16 text-xl text-white bg-green-500 gap-1.5",
+};
 
-interface CreateButtonProps
-  extends ButtonPrimitive.Props, VariantProps<typeof createButtonVariants> {}
+interface CreateButtonProps extends ButtonPrimitive.Props {
+  size?: CreateButtonSize;
+}
 
 export default function CreateButton({
+  size = "small",
   className,
-  size = "default",
+  children,
   ...props
 }: CreateButtonProps) {
   return (
-    <ButtonPrimitive
-      data-slot="button"
-      className={cn(createButtonVariants({ size, className }))}
+    <Button
+      variant="custom"
+      className={cn(createButtonSizeVariants[size], className)}
       {...props}
     >
       <Image src={icPlus} alt="생성 버튼" />
-      {size === "lg" && props.children}
-    </ButtonPrimitive>
+      {size === "large" && children}
+    </Button>
   );
 }
