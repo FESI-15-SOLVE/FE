4e9718693b16773480d4286cbf408751555815f6
pjc0179 <pjc0179@gmail.com>
2026-07-23 15:38:35 +0900

    feat: 네비게이션 링크 추가

---
+++ src/constants/navigation.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  { href: '/meetings', label: '모임 찾기' },
+  { href: '/saved', label: '찜한 모임', hasBadge: true },
+  { href: '/reviews', label: '모든 리뷰' },
+  { href: '/talk', label: '달램 토크' },
+];
