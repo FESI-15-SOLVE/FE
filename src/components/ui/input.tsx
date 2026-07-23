437da6a88451989904925afed5982f323a9fb468
khr <sykhrb793005@naver.com>
2026-07-23 18:02:01 +0900

    ✨feat: input data-invalid 속성 추가

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "rounded-12 w-full min-w-0 border border-transparent bg-slate-50 px-3 text-sm font-normal text-slate-800 transition-colors outline-none placeholder:text-slate-500 md:px-3 md:text-base",
-        "aria-invalid:border-error-500 disabled:bg-input/50 focus-visible:border-green-500 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "aria-invalid:border-error-500 data-invalid:border-error-500 disabled:bg-input/50 focus-visible:border-green-500 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         "file:text-foreground file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium",
         className,
       )}
