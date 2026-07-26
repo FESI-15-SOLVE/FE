42932a6feec62d440ba56e35191c473a57c15d84
khr <sykhrb793005@naver.com>
2026-07-27 03:52:50 +0900

    ✨feat: 찜버튼에서 IconHeart 사용. IconHeartFull, IconHeartEmpty 삭제

--- src/components/ui/wish-button.tsx
+++ src/components/ui/wish-button.tsx
@@ -1,12 +1,12 @@
 import { Button as ButtonPrimitive } from "@base-ui/react/button";
 import { Button } from "@/components/ui/button";
 
-import { IconHeartEmpty, IconHeartFull } from "@/components/icons";
+import { IconHeart } from "@/components/icons";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const wishButtonVariants = cva(
-  "bg-white rounded-full border-1 border-slate-200 [&_svg]:shrink-0 [&_svg]:transition-transform active:[&_svg]:scale-75 aria-pressed:[&_svg]:scale-110",
+  "bg-white rounded-full border-1 border-slate-200 [&_svg]:shrink-0 [&_svg]:transition-transform active:[&_svg]:scale-10 aria-pressed:[&_svg]:scale-100",
   {
     variants: {
       size: {
@@ -32,10 +32,12 @@ export default function WishButton({
   isWished,
   ...props
 }: WishButtonProps) {
-  const Icon = isWished ? IconHeartFull : IconHeartEmpty;
   const iconSize =
     size === "medium" ? "size-6" : size === "large" ? "size-8" : "size-5";
 
+  const emptyHeart = "stroke-slate-400 stroke-2 text-white";
+  const fullHeart =
+    "stroke-2 text-gradient-start-500 stroke-gradient-start-500";
   return (
     <Button
       {...props}
@@ -45,7 +47,7 @@ export default function WishButton({
       aria-label={isWished ? "찜 취소" : "찜하기"}
       className={cn(wishButtonVariants({ size }), className)}
     >
-      <Icon className={iconSize} />
+      <IconHeart className={cn(iconSize, isWished ? fullHeart : emptyHeart)} />
     </Button>
   );
 }
